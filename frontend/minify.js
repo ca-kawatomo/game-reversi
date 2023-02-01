f1427674d545bd94e6613e1f187c17267f565c8d
Tomoyuki Kawano <kawatomo@city.co.jp>
2023-02-01 14:05:48 +0900

    for deploy.

--- frontend/minify.js
+++ frontend/minify.js
@@ -1,16 +1,30 @@
 const UglifyEs = require("uglify-es");
 const fs = require("fs");
 
-const files = fs.readdirSync("./dist", { withFileTypes: true });
-const scriptFile = files.find(dir => dir.name.endsWith(".js"));
-if (!scriptFile) {
-    return;
+
+function minify() {
+  const files = fs.readdirSync("./dist", { withFileTypes: true });
+  const scriptFile = files.find(dir => dir.name.endsWith(".js"));
+  if (!scriptFile) {
+      return;
+  }
+  const scriptFilePath = `./dist/${scriptFile.name}`;
+  const script = fs.readFileSync(scriptFilePath, { encoding: "utf-8" });
+  const minifiedRes = UglifyEs.minify(script);
+  if (minifiedRes.error) {
+      console.error(minifiedRes.error);
+      return;
+  }
+  fs.writeFileSync(scriptFilePath, minifiedRes.code);
 }
-const scriptFilePath = `./dist/${scriptFile.name}`;
-const script = fs.readFileSync(scriptFilePath, { encoding: "utf-8" })
-const minifiedRes = UglifyEs.minify(script);
-if (minifiedRes.error) {
-    console.error(minifiedRes.error);
-    return;
+function adjustPath() {
+    const htmlPath = "dist/index.html"
+    const html = fs.readFileSync(htmlPath, { encoding: "utf-8" })
+                .replace(".auth\\login\\github.", ".auth\\login\\github")
+                .replace(".auth\\login\\aad.", ".auth\\login\\aad")
+                .replace(".auth\\logout.", ".auth\\logout");
+    fs.writeFileSync(htmlPath, html);
 }
-fs.writeFileSync(scriptFilePath, minifiedRes.code);
+
+minify();
+adjustPath();
