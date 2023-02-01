faadf9d9de5602da7421febd68bdc65db6c07062
Tomoyuki Kawano <kawatomo@city.co.jp>
2023-02-01 14:15:55 +0900

    for deploy.

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,5 +2,5 @@ rm -fr dist
 parcel build src/index.html --no-minify
 rm -fr dist/.auth
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype dist/index.html -o dist/index.html
-npx ts-node minify.js
+npx ts-node custom-build.js
 rm dist/*.map
